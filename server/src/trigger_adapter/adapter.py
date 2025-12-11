2fa0497cb9d48345f25f52a030c8703714494a8d
William-Brown5515 <william.brown@stfc.ac.uk>
2025-12-11 15:43:14 +0000

    Change file naming

---
+++ server/src/trigger_adapter/adapter.py
@@ -0,0 +1,52 @@
+import logging
+
+from odin.adapters.adapter import ApiAdapter, ApiAdapterResponse
+from odin.adapters.parameter_tree import ParameterTreeError
+from trigger_adapter.controller import TriggerController, TriggerControllerError
+from tornado.escape import json_decode
+
+class TriggerAdapter(ApiAdapter):
+    def __init__(self, **kwargs):
+        super(TriggerAdapter, self).__init__(**kwargs)
+
+        self.controller = TriggerController()
+        logging.debug("Adapter Loaded")
+
+    def get_controller(self):
+        return self.controller
+
+    def get(self, path, request):
+        try:
+            response = self.controller.get(path)
+            status_code = 200
+        except ParameterTreeError as e:
+            response = {'error': str(e)}
+            status_code = 400
+
+        content_type = 'application/json'
+
+        return ApiAdapterResponse(response, content_type=content_type, status_code=status_code)
+
+    def put(self, path, request):
+        """Handle a HTTP PUT request."""
+        content_type = "application/json"
+
+        try:
+            # Send the set request to the controller
+            data = json_decode(request.body)
+            self.controller.set(path, data)
+            response = self.controller.get(path)
+            status_code = 200
+        except TriggerControllerError as e:
+            response = {"error": str(e)}
+            status_code = 400
+        except (TypeError, ValueError) as e:
+            response = {"error": f"Failed to decode PUT request body: {str(e)}"}
+            status_code = 400
+
+        return ApiAdapterResponse(
+            response, content_type=content_type, status_code=status_code
+        )
+
+    def cleanup(self):
+        self.controller.cleanup()
--- server/src/trigger_adapter/controller.py
+++ server/src/trigger_adapter/controller.py
@@ -6,18 +6,15 @@ from zmq.eventloop.zmqstream import ZMQStream
 
 from odin.adapters.parameter_tree import ParameterTree
 
-class TestController():
+class TriggerControllerError(Exception):
+    pass
+
+class TriggerController():
 
     def __init__(self):
-        self.active = False
-        self.rises = 0
+        self.event_callback = None
 
-        self.param_tree = ParameterTree({
-            'active': (True, None),
-            'rises': (lambda: self.rises, None)
-        })
-
-        self.ctrl_endpoint = f"tcp://192.168.0.13:5555"
+        self.ctrl_endpoint = f"tcp://192.168.0.58:5555"
 
         try:
             self.ctx = zmq.Context()
@@ -31,12 +28,18 @@ class TestController():
         except ZMQError as error:
             logging.error("Error initializing RPC server: %s", error)
 
-    def get(self, path):
-        return self.param_tree.get(path)
+    def register_event(self, event_callback):
+        self.event_callback = event_callback
 
     def handle_receive(self, frames):
         decoded_string = frames[1].decode('utf-8')
-        rises = json.loads(decoded_string)
-        self.rises = rises['times_risen']
-        identity = frames[0]
+        triggers = json.loads(decoded_string)
+
+        if self.event_callback:
+            self.event_callback(frames[0])
+        
+    def reply(self, identity):
         self.ctrl_stream.send_multipart([identity, b'b'])
+
+    def get_reply_method(self):
+        return self.reply
